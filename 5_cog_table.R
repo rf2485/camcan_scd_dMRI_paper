b1b768db57184eac21a78838ae9cfd097c37c881
rf2485 <93285754+rf2485@users.noreply.github.com>
2023-08-27 22:20:29 -0400

    Add files via upload

---
+++ 5_cog_table.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(arsenal)
+
+dwi_over_55_mem = read.delim("dwi_over_55.tsv", tryLogical = FALSE) %>%
+  select(participant_id, SCD, age, sex, homeint_mmse_cal, homeint_storyrecall_i, 
+         homeint_storyrecall_d, objprpos_emotional_mem, objprneu_emotional_mem,
+         objprneg_emotional_mem, prcsn_ss1_vstm, prcsn_ss2_vstm, prcsn_ss3_vstm,
+         prcsn_ss4_vstm) %>%
+  mutate(across(objprpos_emotional_mem:prcsn_ss4_vstm, function(x) x*100)) %>%
+  rename(Age=age, Sex=sex)
+dwi_over_55_mem$SCD <- factor(dwi_over_55_mem$SCD,
+                              levels = c(1,0),
+                              labels = c('SCD', 'Control'))
+attr(dwi_over_55_mem$scd, 'label') <- 'Subjective Cognitive Decline'
+attr(dwi_over_55_mem$homeint_mmse_cal, 'label') <- 'MMSE'
+attr(dwi_over_55_mem$homeint_storyrecall_i, 'label') <- 'Immediate Story Recall'
+attr(dwi_over_55_mem$homeint_storyrecall_d, 'label') <- 'Delayed Story Recall'
+attr(dwi_over_55_mem$objprpos_emotional_mem, 'label') <- 'EM Positive Recognition (%)'
+attr(dwi_over_55_mem$objprneu_emotional_mem, 'label') <- 'EM Neutral Recognition (%)'
+attr(dwi_over_55_mem$objprneg_emotional_mem, 'label') <- 'EM Negative Recognition (%)'
+attr(dwi_over_55_mem$prcsn_ss1_vstm, 'label') <- 'VSTM Set Size 1 Recall (%)'
+attr(dwi_over_55_mem$prcsn_ss2_vstm, 'label') <- 'VSTM Set Size 2 Recall (%)'
+attr(dwi_over_55_mem$prcsn_ss3_vstm, 'label') <- 'VSTM Set Size 3 Recall (%)'
+attr(dwi_over_55_mem$prcsn_ss4_vstm, 'label') <- 'VSTM Set Size 4 Recall (%)'
+results <- 'asis'
+cog_table <- tableby(SCD ~ homeint_mmse_cal + homeint_storyrecall_i + 
+                       homeint_storyrecall_d + objprpos_emotional_mem + 
+                       objprneu_emotional_mem + objprneg_emotional_mem + 
+                       prcsn_ss1_vstm + prcsn_ss2_vstm + prcsn_ss3_vstm + 
+                       prcsn_ss4_vstm, 
+                     data = dwi_over_55_mem, 
+                     numeric.stats = c("N", "meansd", "range"), total = FALSE)
+summary(cog_table, text = TRUE)
+write2word(cog_table, "cog_table.docx")
+
