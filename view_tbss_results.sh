9c9ae9d861707593d1947f99d6addb2c47ce2e1c
Ryn Flaherty <rf2485@nyu.edu>
2023-12-21 13:15:47 -0500

    updating view_tbss_results

--- view_tbss_results.sh
+++ view_tbss_results.sh
@@ -7,8 +7,10 @@ module load fsl/6.0.5
 for metric in "${metric_list[@]}"; do
   for test in "${test_list[@]}"; do
     fsleyes -std1mm all_${metric} lower_cingulum_mask -cm green \
-      ${metric}/${metric}_lc_${test}_tfce_corrp_tstat1 -cm red-yellow -dr 0.949 1 \
-        ${metric}/${metric}_lc_${test}_tfce_corrp_tstat2 -cm blue-lightblue -dr 0.949 1 \
+      ${metric}/${metric}_llc_${test}_tfce_corrp_tstat1 -cm red-yellow -dr 0.949 1 \
+        ${metric}/${metric}_llc_${test}_tfce_corrp_tstat2 -cm blue-lightblue -dr 0.949 1 \
+	        ${metric}/${metric}_rlc_${test}_tfce_corrp_tstat1 -cm red-yellow -dr 0.949 1 \
+	          ${metric}/${metric}_rlc_${test}_tfce_corrp_tstat2 -cm blue-lightblue -dr 0.949 1 \
           
   done
 done
